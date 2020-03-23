193cee4e91f9750bca2755082c77b690507d8dae
Chris Ashman <chrisashman96@gmail.com>
2020-03-23 17:42:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -90,6 +90,7 @@ def self.first_student_in_grade_10
   DB[:conn].execute(sql).map do |row|
     self.new_from_db(row)
   end
+end
 
 def self.first_X_students_in_grade_10(number)
   sql = <<-SQL
@@ -98,3 +99,7 @@ def self.first_X_students_in_grade_10(number)
   
   DB[:conn].execute(sql, number).map do |row|
     self.new_from_db(row)
+  end
+end 
+
+self 
