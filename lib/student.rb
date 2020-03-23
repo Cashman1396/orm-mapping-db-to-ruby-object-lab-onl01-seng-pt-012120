877b3597b68b174f13d918cebd9ce5db41d5284d
Chris Ashman <chrisashman96@gmail.com>
2020-03-23 17:45:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -102,4 +102,15 @@ def self.first_X_students_in_grade_10(number)
   end
 end 
 
-self 
+def self.all_students_in_grade_X(grade)
+  sql = <<-SQL 
+  SELECT * FROM students WHERE grade = ? ORDER BY students.id
+  SQL
+  
+  DB[:conn].execute(sql, grade).map do |row|
+    self.new_from_db(row)
+    end 
+  end
+end
+  
+  
